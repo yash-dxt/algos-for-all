ed7e09497fd36ade8b4785fbb95ead53a8c44afe
Yash Dixit <yashdixitsq@gmail.com>
2021-04-29 14:51:57 +0530

    Linked List: Insertion at start, printing

---
+++ linked_list/insertion.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+// class Node{
+// public:
+//     Node* next;
+//     int data;
+// };
+
+// void push(Node** head_pointer, int data){
+//     Node* new_node = new Node();
+//     new_node->data = data;
+//     new_node->next = *head_pointer;
+//     *head_pointer = new_node;
+// }
+
+// void insertAfter(Node* after_node, int data){
+//     Node* new_node = new Node();
+//     new_node->data = data;
+//     new_node->next = after_node->next;
+//     after_node->next = new_node;
+// }
+
+// void append(Node** head_ref, int data){
+//     Node* new_node = new Node();
+//     new_node->data = data;
+//     new_node->next = NULL;
+//     Node* temp = *head_ref;
+//       if (*head_ref == NULL)
+//     {
+//         *head_ref = new_node;
+//         return;
+//     }
+//     while(temp->next!=NULL){
+//         temp = temp->next;
+//     }
+//     temp->next = new_node;
+//     return;
+// }
+
+// void traverse(Node** head_ref){
+//     while((*head_ref)->next!=NULL){
+//     cout << (*head_ref)->data << " ";
+//     (*head_ref) = (*head_ref)->next;
+//     }
+//     return;
+// }
+
+int main()
+{
+
+    // Node* head = NULL;
+    // push(&head, 3);
+    // traverse(&head);
+    // return 0;
+    cout << "hello";
+}
