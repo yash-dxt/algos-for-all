38c6f9a1618091d95e8273bae8214000a186ff31
Yash Dixit <yashdixitsq@gmail.com>
2021-05-01 14:00:00 +0530

    Linked List: Delete a node

--- linked_list/insertion.cpp
+++ linked_list/insertion.cpp
@@ -56,8 +56,22 @@ void InsertAtPostion(int data, int position, Node **head)
     temp2->next = temp;
 }
 
-void DeleteAtPosition(int n)
+void DeleteAtPosition(int position, Node **head)
 {
+    Node *temp1 = *head;
+    if (position == 1)
+    {
+        *head = temp1->next;
+        delete temp1;
+        return;
+    }
+    for (int i = 0; i < position - 2; i++)
+    {
+        temp1 = temp1->next;
+    }
+    Node *temp2 = temp1->next;
+    temp1->next = temp2->next;
+    delete temp2;
 }
 
 void PrintLinkedList(Node **head)
