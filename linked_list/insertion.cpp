ef56d38aa733694f67626e7e9314c27c63aadc84
Yash Dixit <yashdixitsq@gmail.com>
2021-05-01 11:57:00 +0530

    Linked List: Insert at a specific positon

--- linked_list/insertion.cpp
+++ linked_list/insertion.cpp
@@ -18,6 +18,26 @@ void InsertAtStart(int data, Node **head)
     *head = temp;
 }
 
+void InsertAtPostion(int data, int position, Node **head)
+{
+
+    Node *temp = new Node();
+    temp->data = data;
+    if (position == 1)
+    {
+        temp->next = *head;
+        *head = temp;
+        return;
+    }
+    Node *temp2 = *head;
+    for (int i = 0; i < position - 2; i++)
+    {
+        temp2 = temp2->next;
+    }
+    temp->next = temp2->next;
+    temp2->next = temp;
+}
+
 void PrintLinkedList(Node **head)
 {
     Node *temp = *head;
@@ -32,7 +52,7 @@ void PrintLinkedList(Node **head)
 
 int main()
 {
-    int data, n;
+    int data, n, position;
     cout << "How many inputs? \n";
     cin >> n;
     Node *head = NULL;
@@ -40,7 +60,9 @@ int main()
     {
         cout << "Insert data: \n";
         cin >> data;
-        InsertAtStart(data, &head);
+        cout << "Which position? \n";
+        cin >> position;
+        InsertAtPostion(data, position, &head);
         PrintLinkedList(&head);
     }
 }
